ab7edf59ef3b8bc4505c8066570651da2357b5fd
DaSpood <SpeedyCraft51@gmail.com>
2019-04-25 00:33:04 +0200

    Fix popup cookie

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,13 +1,14 @@
-function togglePopup(value) {
+function togglePopup() {
     var popup = document.getElementById("disclaimer_popup");
-    popup.style.display = value;
+    popup.style.display = 'none';
+    setCookie("popup_seen","true");
 }
 
 var popupcookie = checkCookie("popup_seen","false");
+var popup = document.getElementById("disclaimer_popup");
 if (popupcookie == "false") {
-    setCookie("popup_seen","true");
-    togglePopup('block');
+    popup.style.display = 'block';
 }
 else {
-    togglePopup('none');
+    popup.style.display = 'none';
 }
